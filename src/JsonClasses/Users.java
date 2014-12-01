9c8b249b34936386011ca90fd6f6dc7c3dc3465e
mbmunk <mbauermunk@gmail.com>
2014-12-01 16:14:50 +0100

    ryttet op i users

--- src/JsonClasses/Users.java
+++ src/JsonClasses/Users.java
@@ -5,45 +5,58 @@ public class Users implements java.io.Serializable
 	
 	private static final long serialVersionUId = 1L;
 	private String overallId = "logIn";
+	private int userId;
 	private String email;
 	private String password;
 	private boolean isAdmin;
 	private boolean isActive;
 	
-	public Users(int userId, String email, String password, boolean isAdmin, boolean, isActive) {
-		
+	public Users(int userId, String email, String password, boolean isAdmin, boolean isActive) {
+		this.userId = userId;
+		this.email = email;
+		this.password = password;
+		this.isAdmin = isAdmin;
+		this.isActive = isActive;			
 	}
 	
-	public boolean isActive() {
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public boolean getIsActive() {
 		return isActive;
 	}
-	public void setAuthenticated(boolean isAuthenticated) {
-		this.isAuthenticated = isAuthenticated;
+	public void setActive(boolean isActive) {
+		this.isActive = isActive;
 	}
+	public boolean getIsAdmin() {
+		return isAdmin;
+	}
+	public void setAdmin(boolean isAdmin) {
+		this.isAdmin = isAdmin;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}	
+	
+	
 	
 	public String getOverallId() {
 		return overallId;
 	}
 	public void setOverallId(String overallId) {
 		this.overallId = overallId;
-	}
-	public String getAuthUserEmail() {
-		return email;
-	}
-	public void setAuthUserEmail(String email) {
-		this.email = email;
-	}
-	public String getAuthUserPassword() {
-		return password;
-	}
-	public void setAuthUserPassword(String userName) {
-		this.password = password;
-	}
-	public boolean getAuthUserIsAdmin() {
-		return isAdmin;
-	}
-	public void setAuthUserIsAdmin(boolean isAdmin) {
-		this.isAdmin = isAdmin;
 	}
 
 }
